5baf49efc37b97ccf93bb3d6ce4d809abb73ca5e
Lukasz Reszke <lure@infotjenester.no>
2020-08-19 21:43:17 +0200

    initial commit (with import bug)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import DataSource from 'devextreme/data/data_source';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'AngularWebpackAnalyser';
+  dataSource: DataSource;
+  collapsed = false;
+  contentReady = (e) => {
+    if (!this.collapsed) {
+      this.collapsed = true;
+      e.component.expandRow(['EnviroCare']);
+    }
+  };
+  customizeTooltip = (pointsInfo) => {
+    return { text: parseInt(pointsInfo.originalValue) + '%' };
+  };
+
+  constructor(service: AppService) {
+    this.dataSource = service.getDataSource();
+  }
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import 'devextreme/data/odata/store';
+import DataSource from 'devextreme/data/data_source';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppService {
+  getDataSource() {
+    return new DataSource({
+      store: {
+        type: 'odata',
+        url: 'https://js.devexpress.com/Demos/SalesViewer/odata/DaySaleDtoes',
+        beforeSend: function (request) {
+          request.params.startDate = '2018-05-10';
+          request.params.endDate = '2018-05-15';
+        },
+      },
+    });
+  }
+}
