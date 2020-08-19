48b0d98e1df3dafe8ba5b1901fc2d5f1b6beeca9
Lukasz Reszke <lure@infotjenester.no>
2020-08-19 21:43:30 +0200

    import fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DxDataGridModule } from 'devextreme-angular';
+import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
+// import { DxDataGridModule } from 'devextreme-angular';
 
 @NgModule({
   declarations: [AppComponent],
